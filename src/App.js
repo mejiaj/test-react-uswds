dabcd028d36eca6951da4b851f1a633a389b0beb
James Mejia <james.mejia@gsa.gov>
2022-06-30 15:18:17 -0500

    Separate core style tests into their own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.scss";
 import UswdsCoreTest from "./components/uswds-core-js";
+import UswdsCoreStyles from "./components/uswds-core-styles";
 
 function App() {
   return (
     <div className="container">
       <UswdsCoreTest />
       <hr />
+      <UswdsCoreStyles />
     </div>
   );
 }
--- src/USABanner.js
+++ src/USABanner.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 
-
 function USABanner() {
   const ref = document.body;
 
